f65b12f27355c9e3c25eaf8d654b5549b5ceb2c9
shalom <briantauro7@gmail.com>
2016-04-01 18:25:36 +0530

    initial commit

---
+++ src/jsonDtos/LoginDto.java
@@ -0,0 +1,23 @@
+package jsonDtos;
+
+public class LoginDto {
+
+    public String name;
+    public String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/jsonDtos/StringDto.java
@@ -0,0 +1,16 @@
+package jsonDtos;
+
+/**
+ * Created by shalom on 1/4/16.
+ */
+public class StringDto {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
